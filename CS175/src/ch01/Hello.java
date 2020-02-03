9b27c2781ebd47ab9aa16363d5d272626c83dc71
s1288702 <s1288702@cssemaclin14.monmouth.edu>
2020-02-03 15:24:59 -0500

    House Painting Code

--- CS175/src/ch01/Hello.java
+++ CS175/src/ch01/Hello.java
@@ -5,6 +5,23 @@ public class Hello {
 	public static void main(String[] args) {
 	//This is my first java program
 	System.out.println("Hello");
+	
+	int width = 20;
+	
+	System.out.println("width = " + width);
+	
+	String myName = "Vinny";
+	
+	System.out.println("My name is " + myName);
+	
+	width = 35;
+	
+	myName = "Jeff";
+	
+	System.out.println("width = " + width);
+	
+	System.out.println("My name is " + myName);
+	
 	}
 
 }
---
+++ HousePainting/src/HousePaintingCode.java
@@ -0,0 +1,56 @@
+
+import java.util.Scanner;
+
+public class HousePaintingCode {
+	
+public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in);
+	System.out.print("Please enter the length of the house: ");
+	int length = scan.nextInt();
+	System.out.print("Please enter the width of the house: ");
+	int width = scan.nextInt();
+	System.out.print("Please enter the height of the house: ");
+	int height = scan.nextInt();
+	
+	System.out.print("Please enter the number of windows on the house: ");
+	int windows = scan.nextInt();
+	System.out.print("Please enter the length of the windows on the house: ");
+	int windowLength = scan.nextInt();
+	System.out.print("Please enter the width of the windows on the house: ");
+	int windowWidth = scan.nextInt();
+	
+	System.out.print("Please enter the number of doors on the house: ");
+	int doors = scan.nextInt();
+	System.out.print("Please enter the length of the doors on the house: ");
+	int doorLength = scan.nextInt();
+	System.out.print("Please enter the width of the doors on the house: ");
+	int doorWidth = scan.nextInt();
+	
+	System.out.print("How much does it cost per SqFt: ");
+	int costPerSqFt = scan.nextInt();
+	
+	
+	int windowSqFt = windows * (windowLength * windowWidth);
+	int doorSqFt = doors * (doorLength * doorWidth);
+	int sideSqFt = (2 * (length * width)); 
+	double peaksideSqFt = (2 * (length * width + ((0.5 * (length * (height - width))))));
+	double houseSqFt = (sideSqFt + peaksideSqFt);
+	
+	System.out.println("The square footage of the house is: " + houseSqFt);
+	System.out.println("The square footage of the doors is: " + doorSqFt);
+	System.out.println("The square footage of the windows is: " + windowSqFt);
+	
+	double totalSqFt = (windowSqFt + doorSqFt + houseSqFt);
+	double totalCost = (totalSqFt * costPerSqFt);
+	
+	
+	System.out.println("The total amount of square footage of everything is: " + totalSqFt);
+	
+	System.out.println("The total amount of cost to paint is: $" + totalCost);
+	
+
+	}
+	
+	
+	
+}
