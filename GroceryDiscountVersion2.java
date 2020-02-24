30db78dae6da8d2748ac13b1e5b05d96b078fd92
vinfoss <55250571+vinfoss@users.noreply.github.com>
2020-02-24 15:58:42 -0500

    Update GroceryDiscountVersion2.java

--- GroceryDiscountVersion2.java
+++ GroceryDiscountVersion2.java
@@ -1,6 +1,7 @@
 package code;
 
 import java.text.DecimalFormat;
+
 import java.util.Scanner;
 
 public class GroceryDiscountVersion2 {
@@ -8,19 +9,25 @@ public class GroceryDiscountVersion2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
-		Scanner sc = new Scanner(System.in);
+		boolean start = true;
+		Scanner in = new Scanner(System.in);
 		DecimalFormat df = new DecimalFormat(".#");
-		
-	
-		while (true) {
-			System.out.print("Please enter the cost of your groceries or '0' to quit: ");
-			double cost = sc.nextDouble();
+
+		while (start == true) {
 			
+			System.out.print("Please enter the cost of your groceries or '0' to quit: ");
+			
+			double cost = in.nextDouble();
+			
+			if (cost == 0)
+			{
+				break;
+			}
 			
 			if (cost < 10 || cost > 1000)
 			{
 				System.out.println("You are not eligible for a coupon at this time.");
+				System.out.println("Please enter the cost of your groceries or '0' to quit: ");
 			}
 			else if (cost >= 10 && cost <= 60)
 			{
@@ -38,13 +45,13 @@ public class GroceryDiscountVersion2 {
 			{
 				System.out.println("You win a discount coupon of $" + df.format(0.14 * cost) + "0. (14% of your purchase)");
 			}
-			else if (cost == 0)
+			else
 			{
-				break;
+				System.out.println("Error, non double input was entered therefore the program will end.");
 			}
+			
 		}
-		
-		
+			
 	}
 
 }
